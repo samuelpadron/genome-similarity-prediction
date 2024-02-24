f2d0a7f556a539bb140bcc3c3028044baef27b45
Samuel Padron Alcala <spadron05@gmail.com>
2024-02-24 20:17:09 +0100

    Fixed URL

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -6,14 +6,14 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR" || exit 1
 
 # set environment variables
-source ../.env 2> /dev/null || source .env
+#source ../.env 2> /dev/null || source .env
 
 # default directory to save files in
 DIR="$SCRIPT_DIR"/../data/pair_aligments
 mkdir -p "$DIR"
 
 # download tar file
-curl -LJ0 https://raw.githubusercontent.com/samuelpadron/DNA_alignment_similarity_CSV/pair_alignment/true.csv -o "$DIR/true.csv"
-curl -LJ0 https://raw.githubusercontent.com/samuelpadron/DNA_alignment_similarity_CSV/pair_alignment/false.csv -o "$DIR/false.csv"
+curl -LJ0  https://raw.githubusercontent.com/samuelpadron/DNA_alignment_similarity_CSV/main/pair_alignment/true.csv -o "$DIR/true.csv"
+curl -LJ0 https://raw.githubusercontent.com/samuelpadron/DNA_alignment_similarity_CSV/main/pair_alignment/false.csv -o "$DIR/false.csv"
 # extract tar file
-tar xfv "$DIR"/cifar-10-python.tar.gz -C "$DIR"
+#tar xfv "$DIR"/cifar-10-python.tar.gz -C "$DIR"
