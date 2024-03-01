7aee4ae4a0d9a258435db02a0d1c848e29b096e2
Samuel Padron Alcala <spadron05@gmail.com>
2024-03-01 14:50:14 +0100

    Added custom pair alignment dataset from Google Colab

---
+++ src/dataloaders/datasets/pair_alignment_dataset.py
@@ -0,0 +1,65 @@
+import torch
+from torch.utils.data import DataLoader
+from torchtext import datasets, transforms
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+class SequencePairSimilarityDataset(torch.utils.data.Dataset):
+  def __init__(
+      self,
+      data,
+      max_length,
+      use_padding=None,
+      tokenizer=None,
+      add_eos=False,
+  ):
+    self.data = data
+    self.max_length = max_length
+    self.use_padding = use_padding
+    self.tokenizer = tokenizer
+    self.add_eos = add_eos
+
+  def __len__(self):
+    return len(self.data)
+
+  def __getitem__(self, idx):
+    seq_pair = self.data.iloc[idx]
+    seq1 = seq_pair['sequence_1']
+    seq2 = seq_pair['sequence_2']
+    label = seq_pair['label']
+
+    # #check sequences
+    # print("sequence 1:", seq1)
+    # print("sequence 2:", seq2)
+    # print("label: ", label)
+    # print("__________________________________________________________________")
+
+
+    seq1 = self.tokenizer(seq1,
+            add_special_tokens=False,
+            padding="max_length" if self.use_padding else None,
+            max_length=self.max_length,
+            truncation=True,
+        )  # add cls and eos token (+2)
+    seq2 = self.tokenizer(seq2,
+            add_special_tokens=False,
+            padding="max_length" if self.use_padding else None,
+            max_length=self.max_length,
+            truncation=True,
+        )
+    # check tensors
+    seq1 = seq1["input_ids"]
+    seq2 = seq2["input_ids"]
+
+    # need to handle eos here
+    if self.add_eos:
+      seq1.append(self.tokenizer.sep_token_id)
+      seq2.append(self.tokenizer.sep_token_id)
+
+    # convert to tensor
+    seq1 = torch.LongTensor(seq1)
+    seq2 = torch.LongTensor(seq2)
+
+    label = torch.tensor(label)
+
+    return seq1, seq2, label  #label: 1 ~ true, 0 ~ false
