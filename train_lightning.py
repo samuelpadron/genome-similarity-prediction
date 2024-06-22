63641ec6625191f9b82444565a39cf0c114806f5
Samuel Padron Alcala <spadron05@gmail.com>
2024-06-22 15:04:31 +0200

    Add confusion matrices to logger

--- train_lightning.py
+++ train_lightning.py
@@ -1,13 +1,16 @@
-import pytorch_lightning as pl
-from pytorch_lightning.loggers import TensorBoardLogger
+import sys
 import torch
 import huggingface
 import standalone_hyenadna
+import pytorch_lightning as pl
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pytorch_lightning.loggers import TensorBoardLogger
 from torch.utils.data import DataLoader
 from utils.dataset_splitter import DatasetSplitter
 from src.dataloaders.datasets.pair_alignment_dataset import SequencePairSimilarityDataset
-import os
-import sys
+from sklearn.metrics import confusion_matrix
+
 
 class HyenaDNAModule(pl.LightningModule):
     def __init__(self, pretrained_model_name, backbone_cfg, loss_fn, learning_rate, weight_decay, device):
@@ -22,9 +25,13 @@ class HyenaDNAModule(pl.LightningModule):
         self.loss_fn = loss_fn
         self.learning_rate = learning_rate
         self.weight_decay = weight_decay
-    
+        self.validation_predictions = []
+        self.validation_targets = []
+        
+        
     def forward(self, seq1, seq2):
         return self.model(seq1, seq2)
+    
     
     def training_step(self, batch, batch_idx):
         seq1, seq2, target = batch
@@ -34,18 +41,36 @@ class HyenaDNAModule(pl.LightningModule):
         
         return loss
 
+
     def validation_step(self, batch, batch_idx):
         seq1, seq2, target = batch
         output = self(seq1, seq2)
         loss = self.loss_fn(output, target.float())
         probs = torch.sigmoid(output)
         pred = (probs > 0.5).long()
+        self.validation_predictions.append(pred.cpu())
+        self.validation_targets.append(target.cpu())
         correct = pred.eq(target.view_as(pred)).sum().item()
         accuracy = correct / len(target)
         self.log("val_loss", loss, on_step=False, on_epoch=True)
         self.log("accuracy", accuracy, on_step=False, on_epoch=True)
         
         return {"val_loss": loss, "accuracy": accuracy}
+    
+    
+    def on_validation_epoch_end(self):
+        predictions = torch.cat(self.validation_predictions).numpy()
+        targets = torch.cat(self.validation_targets).numpy()
+        
+        confusion_matrix = confusion_matrix(targets, predictions)
+        
+        fig = plt.figure(figsize=(10, 7))
+        sns.heatmap(confusion_matrix, annot=True, fmt='d', cmap='coolwarm')
+        plt.xlabel('Predicted label')
+        plt.ylabel('Actual label')
+        
+        self.logger.experiment.add_figure("Confusion matrix", fig, self.current_epoch)
+        
         
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(
@@ -88,22 +113,22 @@ class HyenaDNADataModule(pl.LightningDataModule):
         )
     
     def train_dataloader(self):
-        return DataLoader(self.ds_train, batch_size=self.batch_size, shuffle=True)
+        return DataLoader(self.ds_train, batch_size=self.batch_size, shuffle=True, num_workers=4, pin_memory=True)
     
     def val_dataloader(self):
-        return DataLoader(self.ds_test, batch_size=self.batch_size, shuffle=False)
+        return DataLoader(self.ds_test, batch_size=self.batch_size, shuffle=False, num_workers=4, pin_memory=True)
 
 if __name__ == "__main__":
     job_id = sys.argv[1]
     batch_size = int(sys.argv[2]) if len(sys.argv) > 2 else 128
     learning_rate = float(sys.argv[3]) if len(sys.argv) > 3 else 6e-4
     weight_decay = float(sys.argv[4]) if len(sys.argv) > 4 else 0.1
-    num_epochs = 100
     
-    pretrained_model_name = 'hyenadna-small-32k-seqlen'
-    max_length = 13370
+    num_epochs = 100
+    max_length = 32000
     use_padding = 'max_length'
     add_eos = False
+    pretrained_model_name = 'hyenadna-small-32k-seqlen'
     
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     loss_fn = torch.nn.BCEWithLogitsLoss()
@@ -126,7 +151,7 @@ if __name__ == "__main__":
     )
     
     data_module = HyenaDNADataModule(
-        data_path='/vol/csedu-nobackup/project/spadronalcala/pair_alignment/galGal6_1024_13370',
+        data_path='/vol/csedu-nobackup/project/spadronalcala/pair_alignment/panTro6',
         tokenizer=tokenizer,
         batch_size=batch_size,
         max_length=max_length,
@@ -135,14 +160,12 @@ if __name__ == "__main__":
     )
     
     logger = TensorBoardLogger("lightning_logs", name=f"version_{job_id}")
-    print(logger.save_dir)
     
     trainer = pl.Trainer(
         max_epochs=num_epochs,
         logger=logger,
         accelerator='gpu',
         devices=-1,
-        accumulate_grad_batches = 5,
         precision=16,
         strategy='ddp'
     )
